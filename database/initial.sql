f85cd053b54cb2cf9fb9f6ff535246bcf4bbcf8e
hyun-1324 <hdh1324@gmail.com>
2024-06-11 15:38:47 +0300

    add SQLite schema

---
+++ database/initial.sql
@@ -0,0 +1,34 @@
+CREATE TABLE "sessions" (
+  "id" INTEGER PRIMARY KEY,
+  "current_mode" TEXT,
+  "endTime" DATETIME
+);
+
+CREATE TABLE "cars" (
+  "id" INTEGER PRIMARY KEY,
+  "car_number" INTEGER NOT NULL UNIQUE
+);
+
+CREATE TABLE "driver_car_assignments" (
+  "id" INTEGER PRIMARY KEY,
+  "session_id" INTEGER,
+  "car_id" INTEGER,
+  "driverName" TEXT,
+  FOREIGN KEY("session_id") REFERENCES "sessions"("id") ON DELETE CASCADE,
+  FOREIGN KEY("car_id") REFERENCES "cars"("id"),
+  UNIQUE("session_id", "driver_name"),
+  UNIQUE("session_id", "car_id")
+);
+
+CREATE TABLE "lap_times" (
+  "id" INTEGER PRIMARY KEY,
+  "session_id" INTEGER NOT NULL,
+  "car_id" INTEGER NOT NULL,
+  lap_number INTEGER NOT NULL,
+  lap_time DECIMAL(10, 3) NOT NULL,
+  FOREIGN KEY("session_id") REFERENCES "sessions"("id"),
+  FOREIGN KEY("car_id") REFERENCES "cars"("id")
+);
+
+INSERT INTO cars (car_number) VALUES (1), (2), (3), (4), (5), (6), (7), (8);
+
