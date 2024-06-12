4fb064e5b1d1050ab0087b643bc77744499af905
Veera Pylkkänen <veera.pylkkanen@gmail.com>
2024-06-12 09:11:05 +0300

    Add js for html, add classes, add public, change server to use ESM modules

---
+++ public/safety.js
@@ -0,0 +1,14 @@
+/*import { raceModeData } from "./classes.js";
+
+function sendData(modeName, startRace, endSession) {
+    socket.emit('racemode', new raceModeData(modeName, startRace, endSession))
+}
+
+function createButton() {
+const button = document.createElement('button');
+button.innerText = 'Safe';
+document.body.append(button);
+button.addEventListener('click', () => sendData('safe', true, false));
+}
+
+createButton();*/
