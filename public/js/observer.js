073d185547ceb2edc441cabfd1038ecaa5e0cd57
Veera Pylkkänen <veera.pylkkanen@gmail.com>
2024-06-16 14:22:52 +0300

    Add lap-obsrver interface

---
+++ public/js/observer.js
@@ -0,0 +1,41 @@
+import { lapTime } from '../classes.js';
+
+setButtons();
+hideButtons();
+
+function setButtons() {
+    socket.on('next_session', (upcomingSessionData) => {
+        const buttongrid = document.querySelector('#buttongrid');
+        // Clear the buttons from the previous session
+        const buttons = document.querySelectorAll('.carButton');
+        buttons.forEach(button => button.remove());
+        // Create buttons for each driver in the upcoming session
+        for (let i = 1; i < 9; i ++) {
+            if (upcomingSessionData.driverNameList[i] !== '') {
+                const button = document.createElement('button');
+                button.classList.add('carButton');
+                button.innerText = i;
+                button.addEventListener('click', () => {
+                    let time = new Date().getTime();
+                    socket.emit('lapData', new lapTime(i, time));
+                });
+                buttongrid.appendChild(button);
+            };
+        };
+    });
+}
+
+function hideButtons() {
+    const buttongrid = document.querySelector('#buttongrid');
+    const sessionMessage = document.querySelector('#sessionMessage');
+    socket.on('race_mode', (mode) => {
+        if (mode === 'finish') {
+            buttongrid.style.display = 'none';
+            sessionMessage.style.display = 'block';
+
+        } else if (mode === 'safe') {
+            sessionMessage.style.display = 'none';
+        }  
+    });
+}
+
